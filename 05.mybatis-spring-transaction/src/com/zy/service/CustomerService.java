06867bc379a5bd704ee635c143c4a0cfb2d460df
zy <catcatcarrot1207@163.com>
2020-03-17 21:57:26 +0800

    添加事物  third commit

---
+++ 05.mybatis-spring-transaction/src/com/zy/service/CustomerService.java
@@ -0,0 +1,12 @@
+package com.zy.service;
+
+import com.zy.bean.Customer;
+
+public interface CustomerService {
+
+	public void addCustomer(Customer customer);
+	
+	public void test1();
+	
+	public void test2();
+}
